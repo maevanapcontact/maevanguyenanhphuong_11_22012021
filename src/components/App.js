b8b01bea17cc0781b51487045aaf510bded5082b
maevanapcontact <maevanap.contact@gmail.com>
2021-01-25 19:11:52 +0100

    feat: add Host component

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import "../css/App.css";
 import { data } from "../data";
 
 import Gallery from "./Gallery";
+import Host from "./Host";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Gallery images={data[0].pictures} />
+        <Host name={data[0].host.name} img={data[0].host.picture} />
       </div>
     );
   }
---
+++ src/components/Host.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "../css/Host.css";
+
+class Host extends Component {
+  render() {
+    const { name, img } = this.props;
+    const firstname = name.split(" ")[0];
+    const lastname = name.split(" ")[1];
+
+    return (
+      <div className="host">
+        <p>
+          <span>{firstname ? firstname : ""}</span>
+          <span>{lastname ? lastname : ""}</span>
+        </p>
+        <img src={img} alt={name} />
+      </div>
+    );
+  }
+}
+
+export default Host;
