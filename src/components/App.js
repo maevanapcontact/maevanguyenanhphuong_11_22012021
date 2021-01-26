094a42d864fff1d87a6f514879d9cf0fed07ef8f
maevanapcontact <maevanap.contact@gmail.com>
2021-01-26 12:21:16 +0100

    feat: navigation in Gallery

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "../css/App.css";
-// import { data } from "../data";
+import { data } from "../data";
 
 // import Homepage from "./Homepage";
-// import LocationPage from "./LocationPage";
+import LocationPage from "./LocationPage";
 // import ErrorPage from "./ErrorPage";
-import AboutPage from "./AboutPage";
+// import AboutPage from "./AboutPage";
 
 class App extends Component {
   render() {
@@ -13,8 +13,8 @@ class App extends Component {
       <div className="app">
         {/* <ErrorPage /> */}
         {/* <Homepage /> */}
-        {/* <LocationPage locationData={data[0]} /> */}
-        <AboutPage />
+        <LocationPage locationData={data[0]} />
+        {/* <AboutPage /> */}
       </div>
     );
   }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,14 +2,43 @@ import React, { Component } from "react";
 import "../css/Gallery.css";
 
 class Gallery extends Component {
+  state = {
+    currentPicture: 0,
+  };
+
+  goToPreviousImage = () => {
+    this.setState((prevState) => ({
+      currentPicture:
+        prevState.currentPicture === 0
+          ? this.props.images.length - 1
+          : prevState.currentPicture - 1,
+    }));
+  };
+
+  goToNextImage = () => {
+    this.setState((prevState) => ({
+      currentPicture:
+        prevState.currentPicture === this.props.images.length - 1
+          ? 0
+          : prevState.currentPicture + 1,
+    }));
+  };
+
   render() {
     const { images } = this.props;
+    const { currentPicture } = this.state;
 
     return (
       <div className="gallery">
-        <img src={images[0]} alt={`location`} />
-        <span className="fas fa-chevron-left"></span>
-        <span className="fas fa-chevron-right"></span>
+        <img src={images[currentPicture]} alt={`location`} />
+        <span
+          className="fas fa-chevron-left"
+          onClick={() => this.goToPreviousImage()}
+        ></span>
+        <span
+          className="fas fa-chevron-right"
+          onClick={() => this.goToNextImage()}
+        ></span>
       </div>
     );
   }
